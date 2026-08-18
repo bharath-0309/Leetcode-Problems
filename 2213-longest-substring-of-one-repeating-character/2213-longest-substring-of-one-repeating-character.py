bbdc8d9d00ef8695ea320d4bdd787d8f40944365
bharath-0309 <yadagiribharath49@gmail.com>
2026-08-18 20:54:34 +0530

    Time: 5400 ms (71.52%), Space: 124 MB (27.29%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
@@ -2,70 +2,78 @@ class Solution:
     def longestRepeating(self, s, queryCharacters, queryIndices):
         n = len(s)
 
+        pre = [0] * (4 * n)
+        suf = [0] * (4 * n)
+        best = [0] * (4 * n)
         left_char = [''] * (4 * n)
         right_char = [''] * (4 * n)
-        left_len = [0] * (4 * n)
-        right_len = [0] * (4 * n)
-        best = [0] * (4 * n)
+
+        def pull(node):
+            l = node * 2
+            r = l + 1
+
+            left_char[node] = left_char[l]
+            right_char[node] = right_char[r]
+
+            pre[node] = pre[l]
+            suf[node] = suf[r]
+            best[node] = max(best[l], best[r])
+
+            if right_char[l] == left_char[r]:
+                best[node] = max(best[node], suf[l] + pre[r])
+
+                if pre[l] == length[l]:
+                    pre[node] += pre[r]
+
+                if suf[r] == length[r]:
+                    suf[node] += suf[l]
+
         length = [0] * (4 * n)
 
-        def build(node, l, r):
-            length[node] = r - l + 1
+        def build(node, lo, hi):
+            length[node] = hi - lo + 1
 
-            if l == r:
-                left_char[node] = right_char[node] = s[l]
-                left_len[node] = right_len[node] = best[node] = 1
+            if lo == hi:
+                pre[node] = 1
+                suf[node] = 1
+                best[node] = 1
+                left_char[node] = s[lo]
+                right_char[node] = s[lo]
                 return
 
-            mid = (l + r) // 2
-            build(node * 2, l, mid)
-            build(node * 2 + 1, mid + 1, r)
-            merge(node)
+            mid = (lo + hi) // 2
+            build(node * 2, lo, mid)
+            build(node * 2 + 1, mid + 1, hi)
+            pull(node)
 
-        def merge(node):
-            L = node * 2
-            R = node * 2 + 1
-
-            left_char[node] = left_char[L]
-            right_char[node] = right_char[R]
-
-            left_len[node] = left_len[L]
-            if left_len[L] == length[L] and right_char[L] == left_char[R]:
-                left_len[node] += left_len[R]
-
-            right_len[node] = right_len[R]
-            if right_len[R] == length[R] and right_char[L] == left_char[R]:
-                right_len[node] += right_len[L]
-
-            best[node] = max(best[L], best[R])
-
-            if right_char[L] == left_char[R]:
-                best[node] = max(
-                    best[node],
-                    right_len[L] + left_len[R]
-                )
-
-        def update(node, l, r, idx, char):
-            if l == r:
-                left_char[node] = right_char[node] = char
-                left_len[node] = right_len[node] = best[node] = 1
+        def update(node, lo, hi, idx, ch):
+            if lo == hi:
+                left_char[node] = ch
+                right_char[node] = ch
+                pre[node] = 1
+                suf[node] = 1
+                best[node] = 1
                 return
 
-            mid = (l + r) // 2
+            mid = (lo + hi) // 2
 
             if idx <= mid:
-                update(node * 2, l, mid, idx, char)
+                update(node * 2, lo, mid, idx, ch)
             else:
-                update(node * 2 + 1, mid + 1, r, idx, char)
+                update(node * 2 + 1, mid + 1, hi, idx, ch)
 
-            merge(node)
+            pull(node)
 
         build(1, 0, n - 1)
 
         ans = []
 
-        for char, idx in zip(queryCharacters, queryIndices):
-            update(1, 0, n - 1, idx, char)
+        for i in range(len(queryCharacters)):
+            update(1, 0, n - 1, queryIndices[i], queryCharacters[i])
             ans.append(best[1])
 
         return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
